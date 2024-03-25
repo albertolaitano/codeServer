c9e88aacafa310d3ebfaebd69772cf9e59d96103
BlasHeugas <blaas.mdp@hotmail.com>
2024-03-26 00:26:50 +0100

    fixed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import productManager from "./data/fs/ProductManager.fs.js";
+import usersManager from "./data/fs/UsersManager.fs.js";
 
 //server
 const server = express();
@@ -55,6 +56,53 @@ server.get("/api/products/:nid", async (req, res) => {
   try {
     const { nid } = req.params;
     const one = await productManager.readOne(nid);
+    if (one) {
+      return res.status(200).json({
+        response: one,
+        success: true,
+      });
+    } else {
+      const error = new Error("NOT FOUND");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      success: false,
+    });
+  }
+});
+
+server.get("/api/users", async (req, res) => {
+  try {
+    const { role } = req.query;
+    const all = await usersManager.read(role);
+    if (all.length !== 0) {
+      return res.status(200).json({
+        response: all,
+        role,
+        success: true,
+      });
+    } else {
+      const error = new Error("NOT FOUND");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      success: false,
+    });
+  }
+});
+
+server.get("/api/users/:nid", async (req, res) => {
+  try {
+    const { nid } = req.params;
+    const one = await usersManager.readOne(nid);
     if (one) {
       return res.status(200).json({
         response: one,
